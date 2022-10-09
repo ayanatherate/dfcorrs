4677b370d3202e3a9652a953e2c841a0419f3125
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-10-10 02:46:09 +0530

    Add files via upload

---
+++ spearcorr.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+def spearcorr(data,add_cols=[],rem_cols=[],plot_htmp=False):
+    
+    
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from scipy.stats import spearmanr
+    
+    
+    num_cols=list(data._get_numeric_data().columns)
+    num_cols=num_cols+add_cols
+    
+    if len(rem_cols)>0:
+        for i in rem_cols:
+            if i in num_cols:
+                num_cols.remove(i)
+                
+                
+    coef_scores=[]
+
+    for i in num_cols:
+        for j in num_cols:
+            coef, p = spearmanr(data[i], data[j])
+            coef_scores.append(coef)
+            
+    reshape_val=int(np.sqrt(len(coef_scores)))
+    coef_scores=np.array(coef_scores).reshape(-reshape_val,reshape_val)
+    
+    coef_scores_df=pd.DataFrame(coef_scores)
+    coef_scores_df.columns=num_cols
+    coef_scores_df.index=num_cols
+    
+    if plot_htmp==True:
+        plt.figure(figsize=(16,7))
+        sns.heatmap(coef_scores_df)
+        plt.show()
+        
+    return coef_scores_df
+
+import pandas as pd
+data=pd.read_csv(r"C:\Users\User\Desktop\DATASETS_ALL\Movies.csv")
+add_cols=[]
+rem_cols=[]
+print(spearcorr(data))
+    
+
+                
+    
