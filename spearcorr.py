054efa404c81be3f706f671ef1bc89ed14be6ed4
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-10-10 14:58:31 +0530

    Update spearcorr.py

--- spearcorr.py
+++ spearcorr.py
@@ -6,8 +6,7 @@ def cal(data,add_cols=[],rem_cols=[],plot_htmp=False):
     
     import pandas as pd
     import numpy as np
-    import seaborn as sns
-    import matplotlib.pyplot as plt
+    import plotly.express as px
     from scipy.stats import spearmanr
     
     import warnings
