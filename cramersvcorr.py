f78550e2cb1bf629f84fb12a50da1a9df436bc4a
Ayan <59755186+ayanatherate@users.noreply.github.com>
2023-01-24 18:56:50 +0530

    Update cramersvcorr.py

--- cramersvcorr.py
+++ cramersvcorr.py
@@ -9,6 +9,7 @@ def cal(data,add_cols=[],rem_cols=[],plot_htmp=False):
     import numpy as np
     import plotly.express as px
     import scipy.stats as ss
+    from cramers_func import cramers_v
     import warnings
     warnings.filterwarnings('ignore')
     
@@ -32,18 +33,6 @@ def cal(data,add_cols=[],rem_cols=[],plot_htmp=False):
                 cat_cols.remove(i)
             else:
                 pass
-                
-    def cramers_v(x, y):
-        confusion_matrix = pd.crosstab(x,y)
-        chi2 = ss.chi2_contingency(confusion_matrix)[0]
-        n = confusion_matrix.sum().sum()
-        phi2 = chi2/n
-        r,k = confusion_matrix.shape
-        phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
-        rcorr = r-((r-1)**2)/(n-1)
-        kcorr = k-((k-1)**2)/(n-1)
-        
-        return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
                 
                 
     coef_scores=[]
