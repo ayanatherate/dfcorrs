52a65bdd889ffb12351f215ecf3d35a758b74951
Ayan <59755186+ayanatherate@users.noreply.github.com>
2023-03-09 19:08:53 +0530

    r2gg

---
+++ cramers_main.py
@@ -0,0 +1,14 @@
+def cramers_v(x, y):
+    confusion_matrix = pd.crosstab(x,y)
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+    rcorr = r-((r-1)**2)/(n-1)
+    kcorr = k-((k-1)**2)/(n-1)
+        
+    return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
+
+if '__name__'=='__main__':
+    cramers_v(x,y)
