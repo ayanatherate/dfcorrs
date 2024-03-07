c129ff4f14fb555b154a97a270f81c093abae0a8
Ayan <59755186+ayanatherate@users.noreply.github.com>
2024-03-07 12:33:21 +0530

    speed improvement

--- cramersvcorr.py
+++ cramersvcorr.py
@@ -1,67 +1,84 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import scipy.stats as ss
-
-    
 import warnings
 warnings.filterwarnings('ignore')
 
-def cramers_v(x, y):
-    confusion_matrix = pd.crosstab(x,y)
-    chi2 = ss.chi2_contingency(confusion_matrix)[0]
-    n = confusion_matrix.sum().sum()
-    phi2 = chi2/n
-    r,k = confusion_matrix.shape
-    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
-    rcorr = r-((r-1)**2)/(n-1)
-    kcorr = k-((k-1)**2)/(n-1)
+
+class Cramers:
+    
+    def __init__(self):
+        self.coef_scores=list()
         
-    return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
+    def detect_categorical_columns(self, df, threshold_unique=0.05, threshold_distribution=0.95):
+        """ auto-detects categorical columns as per thresholds"""
+        categorical_columns = []
 
+        for col in df.columns:
+            dtype = df[col].dtype
+            unique_count = df[col].nunique()
+            value_counts = df[col].value_counts(normalize=True)
 
-def cal(data,add_cols=[],rem_cols=[],plot_htmp=False):
+            if (dtype == 'object' or dtype.name == 'category') \
+                or (unique_count / len(df) <= threshold_unique) \
+                or (value_counts.max() >= threshold_distribution):
+                categorical_columns.append(col)
+
+        return categorical_columns
     
-    coef_scores=[]
     
-    num_cols=data.corr().columns
-    cat_cols=[cols for cols in data.columns if not cols in num_cols]
-
-    for i in add_cols:
-        if i not in data.columns:
-            print('Added Columns not found in Original Dataframe')
+    def cramers_v(self, x, y):
+        """ computes cramer's v correlation, given two pandas series objects"""
+        confusion_matrix = pd.crosstab(x,y)
+        chi2 = ss.chi2_contingency(confusion_matrix)[0]
+        n = confusion_matrix.sum().sum()
+        phi2 = chi2/n
+        r,k = confusion_matrix.shape
+        phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+        rcorr = r-((r-1)**2)/(n-1)
+        kcorr = k-((k-1)**2)/(n-1)
+        return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1))) 
+    
+    def corr(self,data,add_cols=[],rem_cols=[],plot_htmp=False):
+        
+        """ main function to calculate cramers correlation given the dataframe"""
+        
+        categorical_columns=self.detect_categorical_columns(data, threshold_unique=0.05, threshold_distribution=0.95)
+        
+        if len(add_cols)>0:
+            try:
+                for col in add_cols:
+                    if col in data.columns:
+                        data[col]=data[col].astype('object')
+                        categorical_columns.append(col)
+            except:
+                raise ValueError(f'Unable to convert column type {col} to Object')
+                
+                
+        if len(rem_cols)>0:
+            for col in rem_cols:
+                if col in data.columns:
+                    categorical_columns.remove(col)
+                else:
+                    raise KeyError(f'Unable to locate column {col} in the dataframe')
+                
+        for i in categorical_columns:
+            for j in categorical_columns:
+                coef= self.cramers_v(data[i].fillna('Missing'), data[j].fillna('Missing'))
+                self.coef_scores.append(coef)
+            
+        reshape_val=int(np.sqrt(len(self.coef_scores)))
+        self.coef_scores=np.array(self.coef_scores).reshape(-reshape_val,reshape_val)
+        
+        coef_scores_df=pd.DataFrame(self.coef_scores)
+        coef_scores_df.fillna(0, inplace=True)
+        coef_scores_df.columns=categorical_columns
+        coef_scores_df.index=categorical_columns
+    
+        if plot_htmp==True:
+            fig = px.imshow(coef_scores_df, text_auto=True)
+            fig.show()   
         else:
-            cat_cols.append(i)
-  
-    if len(rem_cols)>0:
-        for i in rem_cols:
-            if i in cat_cols:
-                cat_cols.remove(i)
-            else:
-                pass
-            
-    coef_scores=[]
-
-    for i in cat_cols:
-        for j in cat_cols:
-            coef= cramers_v(data[i], data[j])
-            coef_scores.append(coef)
-            
-    reshape_val=int(np.sqrt(len(coef_scores)))
-    coef_scores=np.array(coef_scores).reshape(-reshape_val,reshape_val)
+            return coef_scores_df
     
-    coef_scores_df=pd.DataFrame(coef_scores)
-    coef_scores_df.columns=cat_cols
-    coef_scores_df.index=cat_cols
-    
-    if plot_htmp==True:
-        fig = px.imshow(coef_scores_df, text_auto=True)
-        fig.show()   
-    else:
-        return coef_scores_df
-    
- if '__name__'=='__main__':
-    cal(data,add_cols=[],rem_cols=[],plot_htmp=False)
-
-
